fb4f6c4ebd481b22cddf0db003ab88e0c75f87b6
piotrkrk11 <piotrkrk11@gmail.com>
2025-06-04 11:35:02 +0000

    ddf

--- main.tf
+++ main.tf
@@ -13,26 +13,26 @@ provider "azurerm" {
 
 terraform {
   backend "azurerm" {
-    resource_group_name  = "rg-PiotrK" #change here
-    storage_account_name = "PiotrK" #change here
+    resource_group_name  = "rg-PiotrK"
+    storage_account_name = "PiotrK"
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
   }
 }
 
 resource "azurerm_service_plan" "example" {
-  name                = "example-app-service-plan" #change here
-  location            = "westeurope" #change here
-  resource_group_name = "rg-PiotrK" #change here
+  name                = "example-app-service-plan"
+  location            = "westeurope"
+  resource_group_name = "rg-PiotrK"
   os_type             = "Linux"
   sku_name            = "P0v3"
 }
 
 
 resource "azurerm_linux_web_app" "example" {
-  name                = "example-webapp-123123i95u8fhwfdsewdwsa" #change here
-  location            = "westeurope" #change here
-  resource_group_name = "example-resources" #change here
+  name                = "example-webapp-123123i95u8fhwfdsewdwsa"
+  location            = "westeurope"
+  resource_group_name = "example-resources"
   service_plan_id     = azurerm_service_plan.example.id
   site_config {}
 }
